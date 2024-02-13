38147527af4f148d076e3548599dfa40dfd6f876
Rajab Ahmad <syrianrm2024@outlook.com>
2024-02-13 22:03:45 +0300

    add some variables lesson1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hi my friends");
+﻿//Console.WriteLine("Hi my friends");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.5;
+char c = 'A';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
